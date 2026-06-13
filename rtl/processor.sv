d7f11cefbee371bf71ac35ac23cc97f9fa29cfe0
Daniel Niu <daniel.n@wustl.edu>
2026-06-13 12:27:58 +0800

    added empty files

---
+++ rtl/processor.sv
@@ -0,0 +1,23 @@
+module processor #(
+    parameter PC_RESET = 32'h00000000
+) (
+    input  logic        clk,
+    input  logic        rst_n,
+
+    // instruction memory interface
+    output logic [31:0] PC_F,
+    input  logic [31:0] instr_F,
+
+    // data memory interface
+    output logic [31:0] mem_addr,
+    output logic [31:0] mem_wr_data,
+    output logic [3:0]  mem_we,
+    output logic        mem_re,
+    input  logic [31:0] mem_rd_data,
+
+    // register file debug port
+    output logic [31:0] dbg_reg_file [32],
+    output logic        halt
+)
+
+endmodule
