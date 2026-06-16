7f5572ebd249c05be3c510a821bb22ece5bfc1be
Daniel Niu <daniel.n@wustl.edu>
2026-06-16 13:17:38 +0800

    updated comparator

--- rtl/comparator.sv
+++ rtl/comparator.sv
@@ -1,27 +1,20 @@
 import processor_pkg::*;
 
 module comparator(
-    input  logic        CompSrc,
     input  logic        CompSign,
     input  logic [1:0]  CompOp,
-    input  logic [31:0] RD1,
-    input  logic [31:0] RD2,
-    input  logic [31:0] ImmExt,
+    input  logic [31:0] Comp1,
+    input  logic [31:0] Comp2,
     output logic        CompResult
 );
 
-    wire [31:0] src1, src2;
-
     always_comb begin
 
-        src1 = RD1;
-        src2 = (CompSrc == COMP_SEL_RD2) ? RD2 : ImmExt;
-
         case (CompOp)
-            COMP_EQ: CompResult = (src1 == src2);
-            COMP_NE: CompResult = (src1 != src2);
-            COMP_LT: CompResult = CompSign ? ($signed(src1) < $signed(src2))  : (src1 < src2);
-            COMP_GE: CompResult = CompSign ? ($signed(src1) >= $signed(src2)) : (src1 >= src2);
+            COMP_EQ: CompResult = (Comp1 == Comp2);
+            COMP_NE: CompResult = (Comp1 != Comp2);
+            COMP_LT: CompResult = CompSign ? ($signed(Comp1) < $signed(Comp2))  : (Comp1 < Comp2);
+            COMP_GE: CompResult = CompSign ? ($signed(Comp1) >= $signed(Comp2)) : (Comp1 >= Comp2);
             default: CompResult = 1'b0;
         endcase
 
