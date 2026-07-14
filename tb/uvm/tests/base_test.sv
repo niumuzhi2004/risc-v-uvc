1383fd8a0951747391d217220611885f08462802
Daniel Niu <daniel.n@wustl.edu>
2026-07-14 16:22:58 +0800

    fixed test termination issue

--- tb/uvm/tests/base_test.sv
+++ tb/uvm/tests/base_test.sv
@@ -2,6 +2,7 @@ class base_test extends uvm_test;
     `uvm_component_utils(base_test)
 
     env environment;
+    uvm_event halt_event;
 
     function new(string name = "base_test", uvm_component parent = null);
         super.new(name, parent);
@@ -10,6 +11,7 @@ class base_test extends uvm_test;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         environment = env::type_id::create("environment", this);
+        halt_event  = uvm_event_pool::get_global("halt_event");
     endfunction
 
 endclass
